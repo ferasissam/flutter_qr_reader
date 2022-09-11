d8be85cff9c0e9e2904e4ddac7e91cd6ee677fc9
Muaath Assaf <muaath.assaf@progressoft.com>
2022-09-11 12:28:18 +0300

    fix: upgrade to android v2

--- android/src/main/java/me/hetian/flutter_qr_reader/factorys/QrReaderFactory.java
+++ android/src/main/java/me/hetian/flutter_qr_reader/factorys/QrReaderFactory.java
@@ -4,22 +4,24 @@ import android.content.Context;
 
 import java.util.Map;
 
-import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.StandardMessageCodec;
 import io.flutter.plugin.platform.PlatformView;
 import io.flutter.plugin.platform.PlatformViewFactory;
 import me.hetian.flutter_qr_reader.views.QrReaderView;
 
 public class QrReaderFactory extends PlatformViewFactory {
-    BinaryMessenger binaryMessenger;
-    public QrReaderFactory(BinaryMessenger binaryMessenger) {
+
+    private PluginRegistry.Registrar registrar;
+
+    public QrReaderFactory(PluginRegistry.Registrar registrar) {
         super(StandardMessageCodec.INSTANCE);
-        this.binaryMessenger = binaryMessenger;
+        this.registrar = registrar;
     }
 
     @Override
     public PlatformView create(Context context, int id, Object args) {
         Map<String, Object> params = (Map<String, Object>) args;
-        return new QrReaderView(context, binaryMessenger, id, params);
+        return new QrReaderView(context, registrar, id, params);
     }
 }
